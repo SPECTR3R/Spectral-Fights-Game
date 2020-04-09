f45d2de3db5dc7c08f17f8bac5eb054e956230c5
Armando del Rio <d-rio@outlook.com>
2020-04-08 21:36:05 -0500

    finished board class

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
-
+// Canvas class
 class Board {
   constructor() {
     // Dimension properties
@@ -8,13 +8,16 @@ class Board {
     this.x = 0;
     this.width = canvas.width;
     this.height = canvas.height;
-    this.speed = 1;
+    this.sound = true;
     // Animation state properties
     this.state = 'load';
     this.interval = undefined;
     // Image of battlefield screen
     this.imgBattlefield = new Image();
-    this.imgBattlefield.src = ' ./images/battlefieldBG.jpg';
+    this.imgBattlefield.src = ' ./images/battlefieldScreenBG.svg';
+    // Image of battlefield load screen
+    this.imgBattlefieldLoad = new Image();
+    this.imgBattlefieldLoad.src = ' ./images/battlefieldLoadScreenBG.svg';
     // Image of instructions screen
     this.imgInstruc = new Image();
     this.imgInstruc.src = ' ./images/instructionScreenBG.svg';
@@ -23,36 +26,46 @@ class Board {
     this.imgSrtart.src = ' ./images/startScreenBG.svg';
     // Inmediately load start screen
     this.imgSrtart.onload = () => {
-      this.startScreen();
+      this.animateStartScreen();
     };
   }
-
-  // Start screen methods
-  startScreen() {
+  // Start screen method
+  animateStartScreen(color) {
     this.clean();
     ctx.drawImage(this.imgSrtart, 0, 0);
-  }
-  animateStartScreen(color) {
-    ctx.fillStyle = '#000000';
-    ctx.fillRect(66, 125, 500, 46);
     ctx.fillStyle = color;
     ctx.font = '55px Espionage';
     ctx.fillText('start game', 80, 160);
   }
-  // Instruction screen methods
-  instructionsScreen() {
+  // Instruction screen method
+  animateInstructionsScreen(color) {
     this.clean();
     ctx.drawImage(this.imgInstruc, 0, 0);
-  }
-  animateInstructionsScreen(color) {
-    ctx.fillStyle = '#000000';
-    ctx.fillRect(620, 218, 140, 22);
     ctx.fillStyle = color;
     ctx.font = '30px Espionage';
     ctx.fillText('BEGIN', 625, 238);
   }
-  // Battlefield load screen methods
-  battlefieldLoadScreen() {
+  // Battlefield load screen method
+  animateBattlefieldLoadScreen() {
+    this.clean();
+    ctx.drawImage(this.imgBattlefieldLoad, 0, 0);
+    ctx.fillStyle = '#ffffff';
+    ctx.font = '100px Metal Gear';
+    if (frames > 0 && frames <= 40) {
+      ctx.fillText('3', 400, 300);
+    } else if (frames > 40 && frames <= 80) {
+      ctx.fillText('2', 400, 300);
+    } else if (frames > 80 && frames <= 120) {
+      ctx.fillText('1', 400, 300);
+    } else if (frames > 120 && frames <= 160) {
+      ctx.fillText('GO!', 330, 300);
+    } else {
+      this.state = 'fight';
+      resetFrames();
+    }
+  }
+  // Battlefield  screen methods
+  animateBattlefieldScreen() {
     this.clean();
     ctx.drawImage(this.imgBattlefield, 0, 0, this.width, this.height);
   }
@@ -63,15 +76,14 @@ class Board {
   gameOver() {
     clearInterval(this.interval);
   }
-  gameRestart() {
-  }
+  gameRestart() {}
   randomNumber(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
   }
 }
 
 // Instances
-const board = new Board(canvas);
+const board = new Board();
 
 // Aux variables
 let frames = 0;
@@ -101,8 +113,8 @@ canvas.addEventListener('mousedown', function (clientX) {
     clickCoordinates[1] > 120 &&
     clickCoordinates[1] < 180
   ) {
-    board.state = 'instructions';
-    frames = 0;
+    board.state = 'instructionsPause';
+    resetFrames();
   }
   if (
     board.state === 'instructionsAnimate' &&
@@ -112,9 +124,33 @@ canvas.addEventListener('mousedown', function (clientX) {
     clickCoordinates[1] < 250
   ) {
     board.state = 'battlefield';
-    frames = 0;
+    resetFrames();
+  }
+  if (
+    board.state === 'fight' &&
+    clickCoordinates[0] > 60 &&
+    clickCoordinates[0] < 135 &&
+    clickCoordinates[1] > 15 &&
+    clickCoordinates[1] < 50
+  ) {
+    ccccc
+  }
+
+  if (
+    board.state === 'fight' &&
+    clickCoordinates[0] > 710 &&
+    clickCoordinates[0] < 800 &&
+    clickCoordinates[1] > 15 &&
+    clickCoordinates[1] < 50
+  ) {
+    board.sound = board.sound ? false : true;
+    console.log(board.sound);
   }
 });
+// helper functions
+function resetFrames() {
+  frames = 0;
+}
 
 let oscilateColor = (frames) => {
   let num = Math.floor(127 * Math.cos(30 * frames) + 127);
@@ -122,31 +158,32 @@ let oscilateColor = (frames) => {
   return color.length < 7 ? '#000000' : color;
 };
 
-// Main functions
-
+// Main function
 const updategame = () => {
   if (board.state === 'load') {
-    frames += 0.001;
-    color = oscilateColor(frames);
+    frames++;
+    color = oscilateColor(frames / 1000);
     board.animateStartScreen(color);
   }
-  if (board.state === 'instructions') {
+  if (board.state === 'instructionsPause') {
     ctx.fillStyle = '#000000';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     frames++;
     if (frames > 50) {
-      board.instructionsScreen();
       board.state = 'instructionsAnimate';
       frames = 0;
-      dir = true;
     }
   }
   if (board.state === 'instructionsAnimate') {
-    frames += 0.001;
-    color = oscilateColor(frames);
+    frames++;
+    color = oscilateColor(frames / 1000);
     board.animateInstructionsScreen(color);
   }
   if (board.state === 'battlefield') {
-    board.battlefieldLoadScreen();
+    frames++;
+    board.animateBattlefieldLoadScreen();
+  }
+  if (board.state === 'fight') {
+    board.animateBattlefieldScreen();
   }
 };
--- js/pruebas.js
+++ js/pruebas.js
@@ -1,9 +1,1 @@
-let whatTimeIsIt = (angle) => {
-  let hour = angle < 30 ? '12' : Math.floor((1 / 30) * angle).toString();
-  let minutes = Math.floor(2 * (angle % 30)).toString();
-  hour = hour.length === 1 ? `0${hour}` : `${hour}`;
-  minutes = minutes.length === 1 ? `0${minutes}` : `${minutes}`;
-  return `${hour}:${minutes}`;
-};
-deg=40
-console.log();
+board.interval = setInterval(updategame, 1000 / 60)
